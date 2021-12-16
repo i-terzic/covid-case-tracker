514cc1a5e34fdfc3c64f494740fdf245f4ebc70e
Ivan Terzic <ivteit00@hs-esslingen.de>
2021-12-16 22:14:03 +0100

    add Routes

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,11 @@ def create_app() -> 'Flask':
     def page_not_found(e) -> 'render_template':
         res = requests.get('https://api.chucknorris.io/jokes/random').text
         joke = json.loads(res).get('value')
-        return render_template('404.html', joke=joke), 404
+        return render_template('404.html', name='404', joke=joke), 404
 
     @app.errorhandler(500)
     def internal_server_error(e) -> 'render_template':
         meme_path = os.path.join('static', '500.png')
-        return render_template('500.html', meme_path=meme_path), 500
+        return render_template('500.html', name='500',  meme_path=meme_path), 500
 
     return app
--- app/views.py
+++ app/views.py
@@ -13,3 +13,9 @@ views = Blueprint("views", __name__)
 @views.route('/home')
 def home() -> 'render_template':
     return render_template('index.html', name='Ivan', title='Home')
+
+
+@views.route('/500')
+def test_500() -> 'render_template':
+    meme_path = os.path.join('static', '500.png')
+    return render_template('500.html', name='500', meme_path=meme_path)
